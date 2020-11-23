b8bc2445b00c72f9fef75c3ea2032823d4677560
Vruttant1403 <Vruttant1403@users.noreply.github.com>
2020-11-23 11:30:10 +0530

    model added

---
+++ src/main/java/com/classnet/model/Program.java
@@ -0,0 +1,28 @@
+package com.classnet.model;
+
+public class Program {
+
+	private String program_id;
+	private String program_name;
+	private int duration;
+	public String getProgram_id() {
+		return program_id;
+	}
+	public void setProgram_id(String program_id) {
+		this.program_id = program_id;
+	}
+	public String getProgram_name() {
+		return program_name;
+	}
+	public void setProgram_name(String program_name) {
+		this.program_name = program_name;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	
+}
---
+++ src/main/java/com/classnet/model/Student.java
@@ -0,0 +1,68 @@
+package com.classnet.model;
+
+import java.util.Date;
+
+public class Student {
+	
+	
+		private String ssid;
+		private String student_name;
+		private String email;
+		private String password;
+		private int type_id;
+		private int status;
+		private Date reg_date;
+		private Program program;
+		
+		
+		public String getSsid() {
+			return ssid;
+		}
+		public void setSsid(String ssid) {
+			this.ssid = ssid;
+		}
+		public String getStudent_name() {
+			return student_name;
+		}
+		public void setStudent_name(String student_name) {
+			this.student_name = student_name;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public int getType_id() {
+			return type_id;
+		}
+		public void setType_id(int type_id) {
+			this.type_id = type_id;
+		}
+		public int getStatus() {
+			return status;
+		}
+		public void setStatus(int status) {
+			this.status = status;
+		}
+		public Date getReg_date() {
+			return reg_date;
+		}
+		public void setReg_date(Date reg_date) {
+			this.reg_date = reg_date;
+		}
+		public Program getProgram() {
+			return program;
+		}
+		public void setProgram(Program program) {
+			this.program = program;
+		}
+		
+	
+}
