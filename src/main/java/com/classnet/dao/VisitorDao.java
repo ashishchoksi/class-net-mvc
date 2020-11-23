6c220c7a6d03f1a65454e922fe6c7611798dd99b
201912026 <zainmansuri7@gmail.com>
2020-11-23 17:38:46 +0530

    registration done! password auto generated!

--- src/main/java/com/classnet/dao/VisitorDao.java
+++ src/main/java/com/classnet/dao/VisitorDao.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Repository;
+import java.util.UUID;
 
 @Repository
 public class VisitorDao {
@@ -47,6 +48,60 @@ public class VisitorDao {
         return false;
     }
     
+ public boolean registration(String email) { 
+        Connection con;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DBConnection.getInstance().getConnection();
+            
+            String sql = "select count(*) from student_detail where email_id = ?";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            int count = rs.getInt(1); 
+//            System.out.println("in Dao" + " " + count);
+            return (count > 0);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(VisitorDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(VisitorDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
+
+ public boolean registration(String email_id,String student_name,String program_id, int student_type_id,String ssid) { 
+     Connection con;
+     try {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         con = DBConnection.getInstance().getConnection();
+         String password = UUID.randomUUID().toString().replaceAll("-", "");
+         String sql = "INSERT INTO `student_detail`(`ssid`, `student_name`, `email_id`, `student_type_id`, `password`, `program_id`) VALUES (?,?,?,?,?,?)";
+         PreparedStatement pstmt = con.prepareStatement(sql);
+         pstmt.setString(1, ssid);
+         pstmt.setString(2, student_name);
+         pstmt.setString(3, email_id);
+         pstmt.setInt(4, student_type_id);
+         pstmt.setString(5, password);
+         pstmt.setString(6, program_id);
+         pstmt.execute();         
+//         return true;
+     } catch (ClassNotFoundException ex) {
+         Logger.getLogger(VisitorDao.class.getName()).log(Level.SEVERE, null, ex);
+         return false;
+     } catch (SQLException ex) {
+         Logger.getLogger(VisitorDao.class.getName()).log(Level.SEVERE, null, ex);
+         return false;
+     } catch (Exception e) {
+		return false;
+	}
+     return true;
+ }
+ 
+
+ // 
     public void abc(){
         System.out.println("in dao");
     }
